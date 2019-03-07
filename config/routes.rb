511c9cf09ddbdd73940ca5efd3985523fd1ecc18
keyylia <clelia.guyon@gmail.com>
2019-03-07 10:24:41 +0100

    index

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   resources :paints
   resources :paintings
   resources :works
+  root to: 'arts#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
