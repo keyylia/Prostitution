2c3716efc6147e0c631240dab48cdcfaf9a5660f
keyylia <clelia.guyon@gmail.com>
2019-03-08 12:38:03 +0100

    V1 finie normalement c'est la panique

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   resources :images
   resources :movs
   resources :arts
-  root to: 'arts#index'
+  root to: 'movs#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
